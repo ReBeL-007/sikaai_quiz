ce4304e380accf542f5af0c3bef0a9c22f74e590
Sanjit01 <sanjitthakuri6750@gmail.com>
2021-09-12 15:47:59 +0545

    small fix

--- resources/views/documents.blade.php
+++ resources/views/documents.blade.php
@@ -7,50 +7,52 @@
 
 @section('content')
 <section class="section document__container">
-    <h3 class="title">Document</h3>
-    <div class="document">
-        <a href="/storage/143/610b922d26a45_Prospectus.pdf" class="document__anker" target="_blank">
-            <div class="individual__pdf">
-                <!-- image container -->
-                <div class="individual__pdf-img">
-                    <img src="https://nepaledufair.com/official-site/pdf.svg" alt="">
-                    <!-- <img src="" alt=""> -->
+    <div class="document__container-content">
+        <h3 class="title">Document</h3>
+        <div class="document">
+            <a href="/storage/143/610b922d26a45_Prospectus.pdf" class="document__anker" target="_blank">
+                <div class="individual__pdf">
+                    <!-- image container -->
+                    <div class="individual__pdf-img">
+                        <img src="https://nepaledufair.com/official-site/pdf.svg" alt="">
+                        <!-- <img src="" alt=""> -->
+                    </div>
+                    <strong class="document__name">
+    
+                        Prospectus
+    
+                    </strong>
                 </div>
-                <strong class="document__name">
-
-                    Prospectus
-
-                </strong>
-            </div>
-        </a>
-        <a href="/storage/143/610b922d26a45_Prospectus.pdf" class="document__anker" target="_blank">
-            <div class="individual__pdf">
-                <!-- image container -->
-                <div class="individual__pdf-img">
-                    <img src="https://nepaledufair.com/official-site/pdf.svg" alt="">
-                    <!-- <img src="" alt=""> -->
+            </a>
+            <a href="/storage/143/610b922d26a45_Prospectus.pdf" class="document__anker" target="_blank">
+                <div class="individual__pdf">
+                    <!-- image container -->
+                    <div class="individual__pdf-img">
+                        <img src="https://nepaledufair.com/official-site/pdf.svg" alt="">
+                        <!-- <img src="" alt=""> -->
+                    </div>
+                    <strong class="document__name">
+    
+                        Prospectus of sanjit thakuri hero 22nd batch
+    
+                    </strong>
                 </div>
-                <strong class="document__name">
-
-                    Prospectus of sanjit thakuri hero 22nd batch
-
-                </strong>
-            </div>
-        </a>
-        <a href="/storage/143/610b922d26a45_Prospectus.pdf" class="document__anker" target="_blank">
-            <div class="individual__pdf">
-                <!-- image container -->
-                <div class="individual__pdf-img">
-                    <img src="https://nepaledufair.com/official-site/pdf.svg" alt="">
-                    <!-- <img src="" alt=""> -->
+            </a>
+            <a href="/storage/143/610b922d26a45_Prospectus.pdf" class="document__anker" target="_blank">
+                <div class="individual__pdf">
+                    <!-- image container -->
+                    <div class="individual__pdf-img">
+                        <img src="https://nepaledufair.com/official-site/pdf.svg" alt="">
+                        <!-- <img src="" alt=""> -->
+                    </div>
+                    <strong class="document__name">
+    
+                        Prospectus
+    
+                    </strong>
                 </div>
-                <strong class="document__name">
-
-                    Prospectus
-
-                </strong>
-            </div>
-        </a>
+            </a>
+        </div>
     </div>
 </section>
 @endsection
